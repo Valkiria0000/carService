45d730fb418c1ab0bceca8de18df909aedf2fd6a
Valkiria0000 <valkiria0000@bk.ru>
2023-04-21 23:13:45 +0300

    remove annimation in section commit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
       <header className={classes.header}>
         <div className={classes.logo}>
             <div className={classes.logo_text}>ТЕХ<span>Ц</span>ЕНТР <br/><span>Гараж</span>
-            <img className={classes.logo_img} src="assets/img/logo.png"/></div>
+            <img className={classes.logo_img} src="assets/img/logo.png" alt=""/></div>
         </div>
         <Menu/>
       </header>
